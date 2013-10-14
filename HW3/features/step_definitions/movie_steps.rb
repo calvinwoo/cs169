4906e637a4309b2e29dd0f209f7be329c8a099e7
saasbook <seung.j@live.com>
2013-10-13 18:06:45 -0700

    I DONE DID THE HOMEWORK

--- HW3/features/step_definitions/movie_steps.rb
+++ HW3/features/step_definitions/movie_steps.rb
@@ -1,11 +1,13 @@
 # Add a declarative step here for populating the DB with movies.
+#Movie.new({"title"=>"BEST", "rating"=>"G", "release_date"=>"25-Nov-1992"})
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
+  
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +16,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert_match(/#{e1}.*#{e2}/m,page.body)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,8 +27,20 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(",")
+  ratings.each do |rating|
+    rating = "ratings_"+rating.delete(" ")
+    if (uncheck)
+        step %{I uncheck "#{rating}"}
+    else
+        step %{I check "#{rating}"}
+    end
+  end
+    
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
+  value = Movie.all.length
+  (page.all("#movies tr").count-1).should == value
 end
